hopefully correct implementation of backprop algorithm for neural network

activation_functions/LeakyReLU.py
@@ -9,5 +9,6 @@ class LeakyReLU:
     def forward(self, inputs):
         outp= np.maximum(self.alpha * inputs, inputs)
         return outp
-    def backward(self, inputs):
+    def derivative(self, inputs):
         outp = np.maximum(self.alpha, inputs)
+        return outp

activation_functions/ReLU.py
@@ -7,7 +7,8 @@ class ReLU:
     def __init__(self):
         pass
     def forward(self, inputs):
-        outp= np.maximum(0, inputs)
-        return outp
-    def backward(self, inputs):
         outp = np.maximum(0, inputs)
+        return outp
+    def derivative(self, inputs):
+        outp = np.maximum(0, inputs)
+        return outp

loss_functons/mean_square_error.py
@@ -4,8 +4,7 @@ class mean_square_error:
         pass
 
     def compute(self, y_true, y_pred):
-        return np.sum((y_true-y_pred)**2)/len(y_true)
+        return np.sum((y_pred - y_true)**2)/len(y_true)
     
     def derivative(self, y_true, y_pred):
-        return 2 * np.sum((y_true-y_pred))/len(y_true)
-
+        return 2 * np.sum((y_pred-y_true))/len(y_true)