082084c6c11cf1df49bac00e1e9f46df08847eaf
Hunter Anderson <hunter.anderson@pnnl.gov>
2023-11-04 12:29:55 -0700

    loss function update, MSE only

--- loss_functons/mean_square_error.py
+++ loss_functons/mean_square_error.py
@@ -6,5 +6,10 @@ class mean_square_error:
     def compute(self, y_true, y_pred):
         return np.sum((y_pred - y_true)**2)/len(y_true)
     
-    def derivative(self, y_true, y_pred):
-        return 2 * np.sum((y_pred-y_true))/len(y_true)
+    def dC_dX(self, X, w, b, y):
+        return np.outer(np.matmul(X, w) + b - y, w) # size(batch_size, n_in)
+
+    def dC_dw(self, X, w, b, y):
+        return 2 * np.sum(np.matmul(np.matmul(X, w) + b - y, X.T), axis=0) / X.shape[0] # size(batch_size, n_out)
+    def dC_db(self, X, w, b, y):
+        return 2 * np.sum(np.matmul(np.matmul(X, w) + b - y), axis=0) / X.shape[0]
